f187e65c3b9c131c919c3bef737581b510cfe971
Unknown <dinowchang@gmail.com>
2018-07-31 00:11:35 +0800

    add kiss begin of layer test case

--- test_gmodBase.py
+++ test_gmodBase.py
@@ -9,6 +9,9 @@ Created on 2018/7/29
 from unittest import TestCase
 from gmod_base import GmodBase
 
+comment_Nothing = ["; Nothing\n"]
+kiss_begin_of_layer = ["; BEGIN_LAYER_OBJECT z=0.300 z_thickness=0.100\n"]
+
 gcode_G28 = ["G28     ; Home all axes\n"]
 gcode_G28_XZ = ["G28 X Z ; Home the X and Z axes\n"]
 
@@ -18,6 +21,14 @@ class TestGmodBase(TestCase):
 
     def tearDown(self):
         del self.gmod
+
+    def test_kiss_parser_begin_of_layer(self):
+        for line in kiss_begin_of_layer:
+            self.gmod.kisslicer162_parser(line, 1)
+        self.assertEqual(self.gmod.begin_of_layer, True)
+        for line in comment_Nothing:
+            self.gmod.kisslicer162_parser(line, 1)
+        self.assertEqual(self.gmod.begin_of_layer, False)
 
     def test_gcode_parser_G28(self):
         for line in gcode_G28:
